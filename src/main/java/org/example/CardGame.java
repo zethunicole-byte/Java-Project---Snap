2acf794fbc608b36716fab856c0ffda287188660
Zethu Nicole Mthethwa <zethunicolemthethwa@MacBookPro.lan>
2026-05-20 16:05:15 +0100

    Add name field to Player class

--- src/main/java/org/example/CardGame.java
+++ src/main/java/org/example/CardGame.java
@@ -52,9 +52,9 @@ public class CardGame {
     // Deal top card
     public Card dealCard() {
 
-        Card topCard = deckOfCards.get(0);
+        Card topCard = deckOfCards.getFirst();
 
-        deckOfCards.remove(0);
+        deckOfCards.removeFirst();
 
         return topCard;
     }
@@ -68,7 +68,7 @@ public class CardGame {
 
         public ArrayList<Card> sortDeckInNumberOrder() {
 
-        Collections.sort(deckOfCards, new Comparator<Card>() {
+        deckOfCards.sort(new Comparator<Card>() {
 
             @Override
             public int compare(Card c1, Card c2) {
@@ -79,3 +79,44 @@ public class CardGame {
             return deckOfCards;
         }
 
+    // This method sorts the deck of cards first by suit, then by value within each suit.
+    // Collections.sort() sorts the list for me.
+    // The Comparator defines the sorting rules by comparing two cards at a time (c1 and c2).
+    // c1.getSuit().compareTo(c2.getSuit()) compares the suits alphabetically,
+    // returning a negative number, zero, or positive number just like the number sort.
+    // If suitCompare == 0, it means both cards have the same suit,
+    // so we then sort by value using c1.getValue() - c2.getValue() (lowest to highest).
+    // If the suits are different, we return suitCompare to sort by suit first.
+
+    // This method shuffles the deck into a random order.
+    // Collections.shuffle() randomly rearranges the list for me.
+    public ArrayList<Card> sortDeckIntoSuits() {
+
+        deckOfCards.sort(new Comparator<Card>() {
+
+            @Override
+            public int compare(Card c1, Card c2) {
+
+                // Compare suit first
+                int suitCompare = c1.getSuit().compareTo(c2.getSuit());
+
+                // If same suit compare value
+                if (suitCompare == 0) {
+                    return c1.getValue() - c2.getValue();
+                }
+
+                return suitCompare;
+            }
+        });
+
+        return deckOfCards;
+    }
+
+    // Shuffle deck
+    public ArrayList<Card> shuffleDeck() {
+
+        Collections.shuffle(deckOfCards);
+
+        return deckOfCards;
+    }
+ }
--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -2,13 +2,15 @@ package org.example;
 
 public class Player {
 
-    private String name;
+    public class Player {
 
-    public Player(String name) {
-        this.name = name;
-    }
+        private String name;
 
-    public String getName() {
-        return name;
+        public Player(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
-}
