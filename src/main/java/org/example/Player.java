c0ad3ad3fc8dafaa30520626801607e29e18878b
Zethu Nicole Mthethwa <zethunicolemthethwa@MacBookPro.lan>
2026-05-20 16:19:00 +0100

    Add constructor to initialise Player name

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -2,15 +2,18 @@ package org.example;
 
 public class Player {
 
-    public class Player {
+    private String name;
 
-        private String name;
-
-        public Player(String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
+    public Player(String name) {
+        this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+}
+
+// This class represents a Player in the card game.
+// A player has a name which is stored as a private variable.
+// Getter method - returns the player's name
+// Constructor - creates a new Player with the given name
