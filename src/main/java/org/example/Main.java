55eb8bc3c3d2a3ff71b468fac72eacfe15e1a3b7
Zethu Nicole Mthethwa <zethunicolemthethwa@MacBookPro.lan>
2026-05-22 11:39:01 +0100

    Added player input and start Snap game in main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,3 +3,19 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Player 1 name: ");
+        Player p1 = new Player(scanner.nextLine());
+
+        System.out.print("Enter Player 2 name: ");
+        Player p2 = new Player(scanner.nextLine());
+
+        Snap game = new Snap("Snap", p1, p2);
+
+        game.playGame();
+    }
+}
