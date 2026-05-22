410322fdf82c4e3af449b3fa68e71cb8908a441a
Zethu Nicole Mthethwa <zethunicolemthethwa@MacBookPro.lan>
2026-05-22 10:52:30 +0100

    Added card dealing logic to Snap game loop

--- src/main/java/org/example/Snap.java
+++ src/main/java/org/example/Snap.java
@@ -17,3 +17,58 @@ public class Snap extends CardGame {
 
         shuffleDeck();
     }
+
+    public void playGame() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Card lastCard = null;
+
+        boolean player1Turn = true;
+
+        System.out.println("SNAP GAME START");
+
+        while (deckOfCards.size() > 0) {
+
+            Player currentPlayer;
+
+            if (player1Turn == true) {
+                currentPlayer = player1;
+            } else {
+                currentPlayer = player2;
+            }
+
+            System.out.println("\n" + currentPlayer.getName() + " press ENTER");
+            scanner.nextLine();
+
+            Card currentCard = dealCard();
+
+            System.out.println(currentPlayer.getName() + " got: " + currentCard);
+
+            if (lastCard != null) {
+
+                if (currentCard.getSymbol().equals(lastCard.getSymbol())) {
+
+                    System.out.println("\nSNAP!");
+
+                    System.out.println(currentPlayer.getName() + " type SNAP quickly!");
+
+                    boolean result = snapTimer();
+
+                    if (result == true) {
+                        System.out.println(currentPlayer.getName() + " wins!");
+                    } else {
+                        System.out.println("Too slow!");
+                        System.out.println("Game over");
+                    }
+
+                    return;
+                }
+            }
+
+            lastCard = currentCard;
+            player1Turn = !player1Turn;
+        }
+
+        System.out.println("No snap happened");
+    }
