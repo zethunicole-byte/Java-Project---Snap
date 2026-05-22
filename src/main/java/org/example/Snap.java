baa3aa657d5d7bfa5e2c2bbe3484e410ab02e8b5
Zethu Nicole Mthethwa <zethunicolemthethwa@MacBookPro.lan>
2026-05-22 10:50:05 +0100

    Added Player objects to Snap game

--- src/main/java/org/example/Snap.java
+++ src/main/java/org/example/Snap.java
@@ -1,4 +1,19 @@
 package org.example;
 
-public class Snap {
-}
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class Snap extends CardGame {
+
+    private Player player1;
+    private Player player2;
+
+    public Snap(String name, Player p1, Player p2) {
+
+        super(name);
+
+        this.player1 = p1;
+        this.player2 = p2;
+
+        shuffleDeck();
+    }
