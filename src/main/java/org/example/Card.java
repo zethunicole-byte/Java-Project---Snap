8ba4f474816660a2de928f7090c0cdb285497f16
Zethu Nicole Mthethwa <zethunicolemthethwa@MacBookPro.lan>
2026-05-22 10:35:57 +0100

    Replaced String suit with Suit enum in Card class

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -2,16 +2,17 @@ package org.example;
 
 public class Card {
 
-    private String suit;
+    private Suit suit;
     private String symbol;
     private int value;
 
-    public Card(String suit, String symbol, int value) {
+    public Card(Suit suit, String symbol, int value) {
         this.suit = suit;
         this.symbol = symbol;
         this.value = value;
     }
-    public String getSuit() {
+
+    public Suit getSuit() {
         return suit;
     }
 
@@ -25,10 +26,8 @@ public class Card {
 
     @Override
     public String toString() {
-        return symbol + suit;
+        return symbol + suit.getSymbol();
     }
-}
-
 
     // create a class called Card
     // Create variables with the card's data - protect the data with private
