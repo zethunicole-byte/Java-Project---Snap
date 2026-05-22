6d365f165b262c97de17a7ea5d593421c19b179b
Zethu Nicole Mthethwa <zethunicolemthethwa@MacBookPro.lan>
2026-05-22 10:42:21 +0100

    Added Suit enum array for deck generation

--- src/main/java/org/example/CardGame.java
+++ src/main/java/org/example/CardGame.java
@@ -17,4 +17,26 @@ public class CardGame {
         createDeck();
     }
 
+    private void createDeck() {
+
+        String[] symbols = {
+                "2", "3", "4", "5", "6", "7",
+                "8", "9", "10", "J", "Q", "K", "A"
+        };
+
+        for (Suit suit : Suit.values()) {
+
+            int value = 2;
+
+            for (String symbol : symbols) {
+
+                deckOfCards.add(
+                        new Card(suit, symbol, value)
+                );
+
+                value++;
+            }
+        }
+    }
+
 
